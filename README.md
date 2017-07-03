5eca6f454feb86ca42a009b7957034fd7f47889a
Gozde Turan <gozdeturan.ie@gmail.com>
2017-07-03 18:37:23 +0100

    Init

---
+++ README.md
@@ -0,0 +1,31 @@
+# Facebook Messenger Echo Bot with ChatbotProxy
+
+An echo bot to handle the authentication and webhook requests sent via facebook for new messenger bots using ChatbotProxy.
+
+This project is developed to work with Heroku with no coding changes required. It implements the standard echo function that is provided by Facebook to provide visual feedback it is working.
+
+This project is written in Node.js and Express to stay in line with the official Facebook docs and make it easier to use their provided examples. And this project is capable to work with/without ChatbotProxy.
+
+## Installation
+
+1) Create your `App` on Facebook Developers platform
+
+2) Subcribe to page events like `messages`, `message_deliveries`, `message_reads`, `messaging_referrals`, `message_echoes`, `conversations`
+
+3) Get a `Page Access Token(PAGE_ACCESS_TOKEN)` for a Facebook Page.
+
+4) Subscribe to that app. (If you use correct credentials, code will automatically subscribe to that page)
+
+5) Create a proxy using Facebook `App Credentials` on [ChatbotProxy](https://www.chatbotproxy.com/proxies/new). It will give `Proxy Url(BASE_URI)` and `Proxy Token(PROXY_TOKEN)`
+
+6) Click deploy button and set `PAGE_ACCESS_TOKEN`, `BASE_URI` and `PROXY_TOKEN`.
+
+Hint: To see full instructions on how to enable in Facebook. [Follow the official guide](https://developers.facebook.com/docs/messenger-platform/quickstart)
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy)
+
+## Licencse
+
+MIT
+
+License note: This code is modified from original repo: https://github.com/culturekings/facebook-messenger-bot-webhook
---
+++ index.js
@@ -0,0 +1,67 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var request = require('request');
+
+app.use(bodyParser.json());
+
+app.set('proxy_token', (process.env.PROXY_TOKEN || ''));
+app.set('base_uri', (process.env.BASE_URI || 'https://graph.facebook.com'));
+app.set('port', (process.env.PORT || 5000));
+app.set('verify_token', (process.env.VERIFY_TOKEN || 'TEST'));
+app.set('page_access_token', (process.env.PAGE_ACCESS_TOKEN || 'NULL'));
+
+app.get('/', function (req, res) {
+  res.send('It works!');
+});
+
+app.get('/messenger/webhook', function (req, res) {
+  if (req.query['hub.verify_token'] === app.get('verify_token')) {
+    res.send(req.query['hub.challenge']);
+  } else {
+    res.send('Error, wrong validation token');
+  }
+});
+
+app.post('/messenger/webhook/', function (req, res) {
+  console.log (req.body);
+  messaging_events = req.body.entry[0].messaging;
+  for (i = 0; i < messaging_events.length; i++) {
+    event = req.body.entry[0].messaging[i];
+    sender = event.sender.id;
+    if (event.message && event.message.text) {
+      text = event.message.text;
+      sendTextMessage(sender, text.substring(0, 200));
+    }
+  }
+  res.sendStatus(200);
+});
+
+function sendTextMessage(sender, text) {
+  messageData = {
+    text:text
+  }
+  request({
+    url: app.get('base_uri') + '/v2.6/me/messages',
+    qs: {access_token:app.get('page_access_token')},
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-Proxy-Token': app.get('proxy_token')
+    },
+    json: {
+      recipient: {id:sender},
+      message: messageData,
+    }
+  }, function(error, response, body) {
+    if (error) {
+      console.log('Error sending message: ', error);
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error);
+    }
+  });
+}
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
